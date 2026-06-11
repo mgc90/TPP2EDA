32fff5fc8fcbcf8fc3e5ab80b7ea2ca9263f84af
Galensonauta <galensoserrano1@gmail.com>
2026-06-11 14:54:57 -0300

    Adaptacion del main al modo admin

--- src/edatpsegundoparcialgrupal/EDATPSegundoParcialGrupal.java
+++ src/edatpsegundoparcialgrupal/EDATPSegundoParcialGrupal.java
@@ -12,23 +12,34 @@ public class EDATPSegundoParcialGrupal {
         ArbolCarreras asistente = new ArbolCarreras();
         boolean salir = false;
         
-        asistente.construirArbolDefault();
-        System.out.println("Árbol construido exitosamente.");
-
         while (!salir) {
-            System.out.println("\n--- ASISTENTE DE CARRERAS ---");
+            System.out.println("\n====== ASISTENTE DE ELECCIÓN DE CARRERAS ======");
+            System.out.println("1. Construir Árbol nuevo (Modo Admin)");
+            System.out.println("2. Construir Árbol por Defecto");
+            System.out.println("3. Realizar Consulta de Carrera");
+            System.out.println("4. Salir");
+            System.out.print("Elija una opción: ");
 
-            System.out.println("1. Realizar Consulta de Carrera");
-            System.out.println("2. Salir");
+             int opcion = sn.nextInt();
             
-            int opcion = sn.nextInt();
             switch (opcion) {
                 case 1:
-                    asistente.iniciarConsulta();
+                    asistente.construirArbolInteractivo(); 
+                    System.out.println("\n¡Árbol interactivo construido exitosamente!");
                     break;
                 case 2:
+                    asistente.construirArbolDefault();
+                    System.out.println("\n¡Árbol por defecto cargado exitosamente!");
+                    break;
+                case 3:
+                    asistente.iniciarConsulta();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del asistente...");
                     salir = true;
                     break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
             }
         }
     }
